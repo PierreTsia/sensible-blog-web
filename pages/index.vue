944835a8989a69949ea56a031bb0e88964c62d80
PierreTsia <pierre.tsiakkaros@gmail.com>
2021-11-21 20:26:17 +0100

    :tada: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <pre>{{ articles }}</pre>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      articles: [],
+    }
+  },
+  async mounted() {
+    await this.test()
+  },
+  methods: {
+    async test() {
+      try {
+        this.articles = await this.$strapi.find('articles')
+      } catch (error) {
+        console.error(error)
+      }
+    },
+  },
+})
+</script>
